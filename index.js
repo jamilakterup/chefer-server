1bd586fa7b37698e18953604a071d635a4e9b928
Md Jamil Akter <jamilakterup@gmail.com>
2023-05-04 15:30:02 +0600

    data added

--- index.js
+++ index.js
@@ -10,16 +10,7 @@ app.get('/', (req, res) => {
     res.send('hello chefer')
 });
 
-app.get('/chef', (req, res) => {
-    res.send(chefData)
-});
 
-app.get('/recipes/:id', (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    const singleData = chefData.find(data => data.id === id);
-    res.send(singleData);
-});
 
 app.listen(port, () => {
     console.log('listening on port', port);
